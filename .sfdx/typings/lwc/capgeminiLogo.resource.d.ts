8fc5af4288fea1893912d7ce773aea3f5058d545
shaikinayath <shaikinayath1265@gmail.com>
2022-09-18 18:57:11 +0530

    intial commit

---
+++ .sfdx/typings/lwc/capgeminiLogo.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/capgeminiLogo" {
+    var capgeminiLogo: string;
+    export default capgeminiLogo;
+}
---
+++ force-app/main/default/lwc/amazonClone/amazonClone.js
@@ -0,0 +1,38 @@
+import { LightningElement,track} from 'lwc';
+import login from '@salesforce/apex/LoginUser.login';
+
+export default class AmazonClone extends LightningElement {
+ 
+    @track isError= false;
+    errorMessage=' ';
+    email;
+     password;
+    handleUsername(event)
+    {
+    this.email = event.target.value;
+    }
+    handlePassword(event)
+    {
+     this.password = event.target.value;
+    }
+    handleLogin()
+    {
+         if(this.email != null && this.password!= null)
+         {
+          login({username:this.email,password: this.password})
+          .then((result)=>
+            {
+                 this.isError=false;
+              console.log('result is',result);
+            })
+            .catch((error)=>{
+                console.log('Error is :',error);
+                this.isError = true;
+                this.errorMessage = error.body.message;
+            })
+         }
+        console.log("Inside Login")
+        console.log("Email val:",this.email)
+        console.log("Password:",this.password)
+    }
+}
